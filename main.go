fffa55bc308f79912bc3289896050e388ca1af21
Mikita Kryvitski <solairerove@gmail.com>
2019-07-24 21:54:28 +0300

    feature: just add some basic example of error handling

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/solairerove/go-error-handling-playground/scan"
 )
 
 func main() {
 	fmt.Println("Error handling playground")
+
+	f, e := fmt.Println("Error?")
+	if e != nil {
+		fmt.Println(e)
+	}
+
+	fmt.Println(f)
+
+	scan.ExampleScan()
 }
---
+++ scan/scan.go
@@ -0,0 +1,32 @@
+package scan
+
+import (
+	"fmt"
+)
+
+// ExampleScan tbd
+func ExampleScan() {
+	fmt.Println("\nExampleScan")
+
+	var answer1, answer2, answer3 string
+
+	fmt.Print("Name:")
+	_, err := fmt.Scan(&answer1)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print("Fav Food:")
+	_, err = fmt.Scan(&answer2)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print("Fav Sport:")
+	_, err = fmt.Scan(&answer3)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(answer1, answer2, answer3)
+}
